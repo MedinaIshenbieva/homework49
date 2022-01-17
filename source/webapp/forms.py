ca02808056a2708f6b2464c108355a73a89e4185
MedinaIshenbieva <medinanurlanova7@gmail.com>
2022-01-17 18:38:55 +0300

    изменила в моделях и в форму

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -2,14 +2,11 @@ from django import forms
 # from django.forms import widgets
 from django.forms import ModelForm
 
-from webapp.models import Type, Status, IssueTracker
+from webapp.models import Type, Status
 
 
-class IssueTrackerForm(forms.Form):
+class TaskForm(forms.Form):
     summary = forms.CharField(max_length=200, required=True, label="Краткое описание")
     description = forms.CharField(max_length=200, required=True, label="Полное описание")
     status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Статус")
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), label="Тип")
-    # class Meta:
-    #     model = IssueTracker
-    #     fields = '__all__'
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, label="Тип")
