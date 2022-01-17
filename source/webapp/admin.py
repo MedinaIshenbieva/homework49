f59d7a108f5eba90e5c6408b04484c314328c51d
MedinaIshenbieva <medinanurlanova7@gmail.com>
2022-01-17 18:39:34 +0300

    изменила в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 
-from webapp.models import IssueTracker, Status, Type
+from webapp.models import Task, Status, Type
 
 
-class IssueTrackerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'status', 'type']
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status']
     list_filter = ['status', 'type']
     search_fields = ['summary', 'status', 'type']
     fields = ['summary', 'description', 'status', 'type']
     readonly_fields = ['created_at', 'updated_at']
 
 
-admin.site.register(IssueTracker, IssueTrackerAdmin)
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Status)
 admin.site.register(Type)
 
